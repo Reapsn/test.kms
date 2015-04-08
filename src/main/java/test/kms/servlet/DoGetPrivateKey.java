7ad2da9b266ed68261dfb8ab9c20372a03be83dc
Reapsn <Reapsn@outlook.com>
2015-04-09 01:46:48 +0800

    init

---
+++ src/main/java/test/kms/servlet/DoGetPrivateKey.java
@@ -0,0 +1,40 @@
+package test.kms.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DoGetPrivateKey extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public DoGetPrivateKey() {
+		super();
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String uri = request.getRequestURI();
+		out.println("<BODY BGCOLOR=\"#FDF5E6\">\n" + "<H2>URI: " + uri
+				+ "</H2>\n" + "</BODY></HTML>");
+		out.close();
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
